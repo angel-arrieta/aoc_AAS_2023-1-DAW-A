34af47212ab4ca04408732165258f9eb020b07bd
angel <aarrieta@iesrafaelalberti.es>
2023-12-15 10:42:36 +0100

    Day1 puzzle complete (1)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import java.io.File
+import java.io.InputStream
 
+fun main() {
+    fun part1(lines: List<String>): MutableList<Int> {
+        val valueslist: MutableList<Int> = mutableListOf()
+        for (line in lines){
+            val digitlist: MutableList<Char> = mutableListOf()
+            for (character in line){
+                val isLetter: Boolean = character.isLetter()
+                if (!isLetter){
+                    digitlist.add(character)
+                    break
+                }
+            }
+            val reverseLine = line.reversed()
+            for (character in reverseLine){
+                val isLetter: Boolean = character.isLetter()
+                if (!isLetter){
+                    digitlist.add(character)
+                    break
+                }
+            }
+            val stringDigits = digitlist.joinToString("")
+            valueslist.add(stringDigits.toInt())
+        }
+        return valueslist
+    }/*
     fun part2(input: List<String>): Int {
         return input.size
+    }*/
+
+    val inputStream: InputStream = File("src/Day01.txt").inputStream()
+    val wholeFile: String = inputStream.bufferedReader().use { it.readText() }
+    val linesList: List<String> = wholeFile.split("\n")
+    val valuesList = part1(linesList)
+    var calibrationSum = 0
+    for (value in valuesList) {
+        calibrationSum += value
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(calibrationSum)
 }
